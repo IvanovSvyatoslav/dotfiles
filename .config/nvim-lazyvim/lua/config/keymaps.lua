b53e92278178d39a510e5f0a54ecce93cb2ea18e
Svyatoslav Ivanov <si.ivanov1212@gmail.com>
2024-03-10 16:37:47 +0300

    Separated system clipboard from vim

--- .config/nvim-lazyvim/lua/config/keymaps.lua
+++ .config/nvim-lazyvim/lua/config/keymaps.lua
@@ -6,20 +6,20 @@ local map = vim.keymap.set
 
 map("i", "jj", "<ESC>", { silent = true })
 
-map("n", "<C-d>", "<C-d>zz", { desc = "Center cursor after moving down half-page" })
-map("n", "<C-u>", "<C-u>zz", { desc = "Center cursor after moving up half-page" })
+map("n", "<C-d>", "<C-d>zz", { desc = "Center cursor after moving down half-page", silent = true })
+map("n", "<C-u>", "<C-u>zz", { desc = "Center cursor after moving up half-page", silent = true })
 
 -- Save pasted word in register when replacing
 map("x", "<leader>p", [["_dP]])
 
 -- Yank into system clipboard
--- map({'n', 'v'}, '<leader>y', '"+y') -- yank motion
--- map({'n', 'v'}, '<leader>Y', '"+Y') -- yank line
+map({ "n", "v" }, "<leader>y", '"+y', { desc = "Yank to system clipboard" }) -- yank motion
+map({ "n", "v" }, "<leader>Y", '"+y$', { desc = "Yank to the end of line to system clipboard" }) -- yank line
 
 -- Delete into system clipboard
--- map({'n', 'v'}, '<leader>d', '"+d') -- delete motion
--- map({'n', 'v'}, '<leader>D', '"+D') -- delete line
+map({ "n", "v" }, "<leader>d", '"+d', { desc = "Delete to system clipboard" }) -- delete motion
+map({ "n", "v" }, "<leader>D", '"+D', { desc = "Delete to the end of line to system clipboard" }) -- delete line
 
 -- Paste from system clipboard
--- map('n', '<leader>p', '"+p')  -- paste after cursor
--- map('n', '<leader>P', '"+P')  -- paste before cursor
+map("n", "<leader>p", '"+p', { desc = "Paste after from system clipboard" }) -- paste after cursor
+map("n", "<leader>P", '"+P', { desc = "Paste before from system clipboard" }) -- paste before cursor
